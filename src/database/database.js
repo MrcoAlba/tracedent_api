107cb23302b9864c589b935a028dd6edd7a440d9
Mrco Alba <mrcoalba@gmail.com>
2022-10-25 13:44:32 -0500

    temporal

--- src/database/database.js
+++ src/database/database.js
@@ -1,5 +1,14 @@
 const { Sequelize } = require('sequelize')
 
+console.log("temporal")
+console.log(process.env.HOST)
+console.log(process.env.USER)
+console.log(process.env.PASSWORD)
+console.log(process.env.DATABASE)
+console.log(process.env.PORT)
+console.log("temporal")
+
+console.log("antes")
 const sequelize = new Sequelize(
     process.env.DATABASE, 
     process.env.USER, 
@@ -8,5 +17,6 @@ const sequelize = new Sequelize(
         host: process.env.HOST,
         dialect: 'postgres'
 })
+console.log("despues")
 
 module.exports = sequelize
