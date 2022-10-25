a73f413d5e45c367abc09972babe20d6469ad39d
Mrco Alba <mrcoalba@gmail.com>
2022-10-25 13:29:17 -0500

    test1

--- src/app.js
+++ src/app.js
@@ -11,6 +11,13 @@ app.use(express.json())
 // routes
 app.use(require('./routes/user.routes'))
 app.use(require('./routes/clinic.routes'))
+console.log("temporal")
+console.log(process.env.HOST)
+console.log(process.env.USER)
+console.log(process.env.PASSWORD)
+console.log(process.env.DATABASE)
+console.log(process.env.PORT)
+console.log("temporal")
 //app.use(require('./routes/dentist.routes'))
 //app.use(require('./routes/patient.routes'))
 
