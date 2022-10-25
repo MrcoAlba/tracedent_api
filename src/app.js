79fb3b2ad25c22b4aaa9fb39ad1f37af15eded3b
mrcoalba <mrcoalba@gmail.com>
2022-10-25 18:58:17 -0500

    testing

--- src/app.js
+++ src/app.js
@@ -8,6 +8,12 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 // routes
+app.use(require('./routes/clinic.routes'))
+app.use(require('./routes/user.routes'))
+app.use(require('./routes/user.routes'))
+app.use(require('./routes/user.routes'))
+app.use(require('./routes/user.routes'))
+app.use(require('./routes/user.routes'))
 app.use(require('./routes/user.routes'))
 app.use(require('./routes/clinic.routes'))
 app.use(require('./routes/dentist.routes'))
