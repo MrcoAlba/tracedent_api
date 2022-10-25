e623668310b77afd2753a9fe59852c64c14102ce
Mrco Alba <mrcoalba@gmail.com>
2022-10-25 14:32:38 -0500

    temporal

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const port = process.env.PORT || 4000
 async function main() {
     try{
         console.log("11111")
-        await sequelize.authenticate()
+        await sequelize.sync()
         console.log("22222")
-        
+
         app.listen(port)
         console.log('Server on port', port)
     }catch (err){
