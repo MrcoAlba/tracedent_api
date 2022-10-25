5f1ddd5e9b483679379505c30d19492ac659a02b
Mrco Alba <mrcoalba@gmail.com>
2022-10-25 14:41:17 -0500

    temporal

--- src/database/database.js
+++ src/database/database.js
@@ -1,14 +1,5 @@
 const { Sequelize } = require('sequelize')
 
-console.log("temporal")
-console.log(process.env.HOST)
-console.log(process.env.USER)
-console.log(process.env.PASSWORD)
-console.log(process.env.DATABASE)
-console.log(process.env.PORT)
-console.log("temporal")
-
-console.log("antes")
 const sequelize = new Sequelize(
     process.env.DATABASE_URL,{
         dialectOptions: {
@@ -16,7 +7,7 @@ const sequelize = new Sequelize(
                 rejectUnauthorized: false,
             }
         }
-    })
-console.log("despues")
+    }
+)
 
 module.exports = sequelize
