76b17870e3186b44c8bc9947819dd07320e1f6ee
Mrco Alba <mrcoalba@gmail.com>
2022-10-25 13:49:01 -0500

    temporal

--- src/database/database.js
+++ src/database/database.js
@@ -9,14 +9,15 @@ console.log(process.env.PORT)
 console.log("temporal")
 
 console.log("antes")
-const sequelize = new Sequelize(
+/*const sequelize = new Sequelize(
     process.env.DATABASE, 
     process.env.USER, 
     process.env.PASSWORD,
     {
         host: process.env.HOST,
         dialect: 'postgres'
-})
+})*/
+const sequelize = new Sequelize(process.env.URI)
 console.log("despues")
 
 module.exports = sequelize
