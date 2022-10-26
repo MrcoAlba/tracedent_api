0de378c40e41f8af5df32ed6a40288a0990f358e
Mrco Alba <mrcoalba@gmail.com>
2022-10-26 02:18:01 -0500

    testing user routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,10 +6,7 @@ const { DataTypes } = require('sequelize')
 const getAllUsers = async (req, res) => {
     try {
         const user = await usersSchema.findAll({
-            attributes: ['id_user', 'user_type', 'mail', 'pswd', 'phone_number', 'subscription', 'district', 'direction', 'latitude', 'longitude']
-            /*
             attributes: ['id_user', 'user_type', 'phone_number', 'subscription', 'district', 'direction', 'latitude', 'longitude'] 
-            */
         })
         res.status(200).send(user)
     } catch (error) {
@@ -20,24 +17,20 @@ const getAllUsers = async (req, res) => {
 const patchUserById = async (req, res) => {
     try {
         const id = req.params.id
-        /*
+        
         const user = await usersSchema.update(
             { 
                 subscription: true,
-                updatedAt: DataTypes.NOW()
             }, {
                 where: {
                     id_user: id
                 }
             }
         )
-        res.status(200).json(user)
-        */
-        res.status(200).json(id)
+        res.status(200).send(user)
     } catch (error) {
         res.status(500).send(error)
     }
-    res.send('patchUserById')
 }
 
 module.exports = { getAllUsers, patchUserById }
