50bd2dc829ea54d3a94a83d9d221d3cdf74dae41
Mrco Alba <mrcoalba@gmail.com>
2022-10-26 01:16:56 -0500

    testing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+/*
 require('dotenv').config()
 const app = require('./app')
 const sequelize = require('./database/database')
+*/
 const port = process.env.PORT || 4000
 
 async function main() {
     try{
+        /*
         await sequelize.sync()
+        */
         app.listen(port)
         console.log('Server on port', port)
     }catch (err){
