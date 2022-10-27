f6aa935087d90b66506750ac361635d5371837e8
mrcoalba <mrcoalba@gmail.com>
2022-10-27 03:46:55 -0500

    recruitment routes

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(require('./routes/patient.routes'))
 
 app.use(require('./routes/recruitment.routes'))
 //
-//app.use(require('./routes/speciality.routes'))
+app.use(require('./routes/speciality.routes'))
 //app.use(require('./routes/dentistSpecialities.routes'))
 
 //app.use(require('./routes/comment.routes'))
