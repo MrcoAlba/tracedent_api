f6ecfa512e1dff857d5d332b81c92571679f04aa
mrcoalba <mrcoalba@gmail.com>
2022-10-27 09:53:11 -0500

    recruitment routes

--- src/controllers/dentistSpecialities.controller.js
+++ src/controllers/dentistSpecialities.controller.js
@@ -1,4 +1,16 @@
 const dentistSpecialitiesSchema = require('../models/dentistSpecialities')
 
+// READ     -> GET ALL DENTISTSPECIALITIES RELATIONS
+const getAllDentistSpecialitiesList = async (req, res) => {
+    try {
+        // GET BODY`
+        const dentistSpecialities = await dentistSpecialitiesSchema.findAll({
 
-module.exports = {  }
+        })
+        res.status(200).send({"message":dentistSpecialities})
+    } catch (error) {
+        res.status(500).send({"message":error})
+    }
+}
+
+module.exports = { getAllDentistSpecialitiesList }
--- src/routes/dentistSpecialities.routes.js
+++ src/routes/dentistSpecialities.routes.js
@@ -1,4 +1,10 @@
 const { Router } = require('express')
 const router = Router()
+const{
+    getAllDentistSpecialitiesList
+} = require('../controllers/dentistSpecialities.controller')
+
+// READ     -> GET ALL DENTISTSPECIALITIES RELATIONS
+router.get      ('/dentistspeciality'      ,   getAllDentistSpecialitiesList     )
 
 module.exports = router
