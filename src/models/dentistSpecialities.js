c8a028c0ba2e2db92a2bfcde5f47b07797406ba6
mrcoalba <mrcoalba@gmail.com>
2022-10-27 09:59:23 -0500

    recruitment routes

--- src/models/dentistSpecialities.js
+++ src/models/dentistSpecialities.js
@@ -6,6 +6,11 @@ const dentistSpecialitiesSchema = sequelize.define('dentistSpecialities',{
         type:               DataTypes.UUID,
         primaryKey:         true,
         defaultValue:       DataTypes.UUIDV1
+    },
+    beg_date:{
+        type:               DATE,
+        allowNull:          false,
+        defaultValue:       sequelize.fn('NOW')
     }
 },{
     freezeTableName: true,
