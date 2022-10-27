2b74f024436224cd37336bf61e332f162f74316e
mrcoalba <mrcoalba@gmail.com>
2022-10-27 03:37:42 -0500

    recruitment routes

--- src/routes/speciality.routes.js
+++ src/routes/speciality.routes.js
@@ -1,6 +1,10 @@
 const { Router } = require('express')
 const router = Router()
+const { 
+    postDentist
+} = require('../controllers/speciality.controller')
 
-
+// CREATE   -> POST A NEW SPECIALITY
+router.post     ('/speciality'         ,   postDentist         )
 
 module.exports = router
