09a9859b90479f12757b806149851077f67b1454
mrcoalba <mrcoalba@gmail.com>
2022-11-05 20:30:33 -0500

    fixing api

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,8 +5,8 @@ const { Op } = require('sequelize')
 const getAllUsers = async (req, res) => {
     try {
         // Get query parameters
-        var offset = req.query.offset
-        var limit = req.query.limit
+        var offset  = req.query.offset
+        var limit   = req.query.limit
         // Validate if query parameters are valid
         if (!containsOnlyNumbers(offset) || !containsOnlyNumbers(limit)){
             offset = null
@@ -34,18 +34,40 @@ const getAllUsers = async (req, res) => {
         // If there was an error, send a message and the error object
         res.status(400).send({
             message:"ERROR",
-            response:error
+            response:error,
+            meta:{total: null, count:null, offset: null, limit: null}
         })
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // UPDATE   -> MODIFY THE SUBSCRIPTION TO TRUE BY ID
 const patchUserSubById = async (req, res) => {
     try {
         // Get path parameters
         const id = req.params.id
         // Update the user if it's not a patient
-        const updatedUsers = await usersSchema.update(
-            {
+        const updatedUsers = await usersSchema.update({
                 subscription: true,
             }, {
                 where: {
@@ -59,37 +81,106 @@ const patchUserSubById = async (req, res) => {
         // Send the response
         res.status(200).send({
             message:"OK",
-            data:updatedUsers
+            data:updatedUsers,
+            meta:{total: null, count:null, offset: null, limit: null}
         })
     } catch (error) {
-        // Send the response
-        res.status(200).send({
+        // If there was an error, send a message and the error object
+        res.status(400).send({
             message:"ERROR!",
-            data:error
+            data:error,
+            meta:{total: null, count:null, offset: null, limit: null}
         })
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // LOGIN   -> RETURN 1 IF LOGIN TRUE
 const loginMailPass = async (req, res) => {
     try {
-        // GET BODY
-        const {
-            mail, password
-        } = req.body
-        const user = await usersSchema.findOne({
-            attributes: ['id_user', 'user_type', 'phone_number', 'subscription', 'district', 'direction', 'latitude', 'longitude'],
-            where: {
-                mail: mail,
-                pswd: password
-            }
-        })
-        res.status(200).send({cod:1,response:user})
+        // Get body parameters
+        const mail      = req.body.mail
+        const password  = req.body.password
+        // Find the required user and check credentials
+        const userInformation = await usersSchema.findOne({
+                attributes: ['id_user', 'user_type', 'phone_number', 'subscription', 'district', 'direction', 'latitude', 'longitude'],
+                where: {
+                    mail: mail,
+                    pswd: password
+        }})
+        if (userInformation!=null){
+            // Send the response
+            res.status(200).send({
+                message:"OK",
+                data:userInformation,
+                meta:{total: null, count:null, offset: null, limit: null}
+            })
+        }else{
+            res.status(200).send({
+                message:"BAD CREDENTIALS",
+                data:userInformation,
+                meta:{total: null, count:null, offset: null, limit: null}
+            })
+        }
+        
     } catch (error) {
-        // Due to a simple change in a values, if the return is 0, 
-        //it means that the value wasn't modified
-        res.status(500).send({cod:0,response:error})
+        // If there was an error, send a message and the error object
+        res.status(400).send({
+            message:"ERROR!",
+            data:error,
+            meta:{total: null, count:null, offset: null, limit: null}
+        })
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // EMAIL CHECK -> RETURN 1 IF MAIL DOESN'T EXISTS
 const emailCheckForExistance = async (req, res) => {
     try {
