bf4359699555e7109fd61d2e7ad35c8d21a91d4d
mrcoalba <mrcoalba@gmail.com>
2022-11-16 18:00:36 -0500

    fixing api

--- src/controllers/clinic.controller.js
+++ src/controllers/clinic.controller.js
@@ -49,7 +49,7 @@ const getAllClinics = async (req, res) => {
             console.log("1")
             console.log("1")
             const respuesta1 = await sequelize.query(
-                "SELECT * FROM `clinic`", { 
+                "SELECT * FROM clinic", { 
                     type: QueryTypes.SELECT 
                 }
             )
