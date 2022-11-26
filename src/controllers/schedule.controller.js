404ca6ad958a8a0b40662c23381673156036e101
mrcoalba <mrcoalba@gmail.com>
2022-11-26 00:03:18 -0500

    fixing api

--- src/controllers/schedule.controller.js
+++ src/controllers/schedule.controller.js
@@ -43,6 +43,7 @@ const getAllSchedules = async (req, res) => {
 //READ      -> GET ALL SCHEDULES BY DENTIST ID (ALSO STATUS AND CLINIC ID)
 const getAllSchedulesByDentistId = async (req, res) => {
     try {
+        /*
         // Get query parameters
         const offset        = isNaN(parseInt(req.query.offset))         ? null : parseInt(req.query.offset)
         const limit         = isNaN(parseInt(req.query.limit))          ? null : parseInt(req.query.limit)
