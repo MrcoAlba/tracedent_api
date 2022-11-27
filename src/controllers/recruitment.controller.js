b6be5593dfa9bc17760d8546d5c77f0a33d24ee7
mrcoalba <mrcoalba@gmail.com>
2022-11-27 11:28:28 -0500

    fixing api

--- src/controllers/recruitment.controller.js
+++ src/controllers/recruitment.controller.js
@@ -1,6 +1,6 @@
-const { Op }                    = require('sequelize')
+const { Op } = require('sequelize')
 
-const recruitmentSchema         = require('../models/recruitment')
+const recruitmentSchema = require('../models/recruitment')
 
 
 
@@ -11,10 +11,10 @@ const recruitmentSchema         = require('../models/recruitment')
 const getAllRecruitments = async (req, res) => {
     try {
         // Get query parameters
-        const offset    = isNaN(parseInt(req.query.offset))                   ? null : parseInt(req.query.offset)
-        const limit     = isNaN(parseInt(req.query.limit))                    ? null : parseInt(req.query.limit)
-        const id_clinic = req.query.id_clinic == "undefined" ? ""   : req.query.id_clinic
-        const id_dentist= req.query.id_dentist== "undefined" ? ""   : req.query.id_dentist
+        const offset = isNaN(parseInt(req.query.offset)) ? null : parseInt(req.query.offset)
+        const limit = isNaN(parseInt(req.query.limit)) ? null : parseInt(req.query.limit)
+        const id_clinic = req.query.id_clinic == "undefined" ? "" : req.query.id_clinic
+        const id_dentist = req.query.id_dentist == "undefined" ? "" : req.query.id_dentist
         // Request all the recruitment information
         var recruitment = null
         console.log("1")
@@ -25,35 +25,35 @@ const getAllRecruitments = async (req, res) => {
         console.log("2")
         console.log("2")
         console.log("2")
-        if (id_clinic.length == 36){
+        if (id_clinic.length == 36) {
             recruitment = await recruitmentSchema.findAndCountAll({
-                attributes: ['id_recruitment', 'beg_date', 'end_date','sttus', 'id_clinic', 'id_dentist'],
+                attributes: ['id_recruitment', 'beg_date', 'end_date', 'sttus', 'id_clinic', 'id_dentist'],
                 order: [['sttus', 'ASC']],
-                where:{
-                    id_clinic:id_clinic
+                where: {
+                    id_clinic: id_clinic
                 },
-                offset:     offset,
-                limit :     limit,
-                subQuery:   false
+                offset: offset,
+                limit: limit,
+                subQuery: false
             })
-        }else if(id_dentist.length == 36){
+        } else if (id_dentist.length == 36) {
             recruitment = await recruitmentSchema.findAndCountAll({
-                attributes: ['id_recruitment', 'beg_date', 'end_date','sttus', 'id_clinic', 'id_dentist'],
+                attributes: ['id_recruitment', 'beg_date', 'end_date', 'sttus', 'id_clinic', 'id_dentist'],
                 order: [['sttus', 'ASC']],
-                where:{
-                    id_dentist:id_dentist
+                where: {
+                    id_dentist: id_dentist
                 },
-                offset:     offset,
-                limit :     limit,
-                subQuery:   false
+                offset: offset,
+                limit: limit,
+                subQuery: false
             })
-        }else{
+        } else {
             recruitment = await recruitmentSchema.findAndCountAll({
-                attributes: ['id_recruitment', 'beg_date', 'end_date','sttus', 'id_clinic', 'id_dentist'],
+                attributes: ['id_recruitment', 'beg_date', 'end_date', 'sttus', 'id_clinic', 'id_dentist'],
                 order: [['sttus', 'ASC']],
-                offset:     offset,
-                limit :     limit,
-                subQuery:   false
+                offset: offset,
+                limit: limit,
+                subQuery: false
             })
         }
         // Get the data, total and count information
@@ -62,16 +62,64 @@ const getAllRecruitments = async (req, res) => {
         const count = data.length
         // Send the response
         res.status(200).send({
-            message:"OK",
-            data:data,
-            meta:{total: total, count:count, offset: offset, limit: limit}
+            message: "OK",
+            data: data,
+            meta: { total: total, count: count, offset: offset, limit: limit }
         })
     } catch (error) {
         // If there was an error, send a message and the error object
         res.status(400).send({
-            message:"ERROR",
-            response:error,
-            meta:{total: null, count:null, offset: null, limit: null}
+            message: "ERROR",
+            response: error,
+            meta: { total: null, count: null, offset: null, limit: null }
+        })
+    }
+}
+//STATUS CHANGE -> CHANGE RECRUITMENT STATUS
+const changeRecruitmentStatusById = async (req, res) => {
+    try {
+        // Get path parameters
+        const id_recruitment = req.params.id
+        // Get query parameters
+        const status = isNaN(parseInt(req.query.status)) ? 5 : parseInt(req.query.status)
+
+        if (status >= 0 && status <= 4) {
+            const schedule = await recruitmentSchema.update({
+                sttus: status
+            }, {
+                where: {
+                    id_recruitment: id_recruitment
+                }
+            })
+            // Send the response
+            console.log("SCHEDULE UPDATED")
+            console.log("SCHEDULE UPDATED")
+            console.log("SCHEDULE UPDATED")
+            console.log(schedule)
+            console.log(schedule)
+            console.log(schedule)
+            console.log("SCHEDULE UPDATED")
+            console.log("SCHEDULE UPDATED")
+            console.log("SCHEDULE UPDATED")
+            res.status(200).send({
+                message: "SCHEDULE UPDATED",
+                data: [true],
+                meta: { total: null, count: null, offset: null, limit: null }
+            })
+        } else {
+            // Send the response
+            res.status(500).send({
+                message: "SCHEDULE NOT UPDATED",
+                data: [false],
+                meta: { total: null, count: null, offset: null, limit: null }
+            })
+        }
+    } catch (error) {
+        // If there was an error, send a message and the error object
+        res.status(400).send({
+            message: "ERROR",
+            response: error,
+            meta: { total: null, count: null, offset: null, limit: null }
         })
     }
 }
@@ -85,13 +133,6 @@ const getAllRecruitments = async (req, res) => {
 
 
 
-
-
-
-
-
-
-
-module.exports = { 
-    getAllRecruitments 
+module.exports = {
+    getAllRecruitments, changeRecruitmentStatusById
 }
--- src/routes/recruitment.routes.js
+++ src/routes/recruitment.routes.js
@@ -1,13 +1,14 @@
 const { Router }    = require('express')
 const router        = Router()
 const { 
-    getAllRecruitments
+    getAllRecruitments, changeRecruitmentStatusById
 }                   = require('../controllers/recruitment.controller')
 
 //READ      -> GET AL RECRUITMENTSS
 router.get      ('/recruitment'     ,   getAllRecruitments      )
 
-
+//STATUS CHANGE -> CHANGE RECRUITMENT STATUS
+router.post      ('/recruitment/change/:id', changeRecruitmentStatusById)
 
 
 
